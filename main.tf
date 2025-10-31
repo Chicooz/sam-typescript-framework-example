888219d23207c9cbc31ab2888ae7142b0251df46
Sherif Negm <sherif@sherifnegm.com>
2025-10-31 20:08:54 +0200

    feat(infra): Infra: Terraform configuration (additive-only)
    [BuildGPT] Auto-generated for stone: infra.terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-FILE: main.tf
 # Main Terraform configuration file
 
 provider "aws" {
@@ -13,37 +12,3 @@ resource "aws_s3_bucket" "my_bucket" {
 resource "aws_s3_bucket" "additional_bucket" {
   bucket = "my-additional-bucket-name"
 }
-
-FILE: variables.tf
-# Variables for Terraform configuration
-
-variable "bucket_name" {
-  description = "The name of the S3 bucket"
-  type        = string
-}
-
-# New variable for additional bucket name
-variable "additional_bucket_name" {
-  description = "The name of the additional S3 bucket"
-  type        = string
-}
-
-FILE: outputs.tf
-# Outputs for Terraform configuration
-
-output "bucket_id" {
-  value = aws_s3_bucket.my_bucket.id
-}
-
-# New output for additional bucket ID
-output "additional_bucket_id" {
-  value = aws_s3_bucket.additional_bucket.id
-}
-
-FILE: terraform.tfvars
-# Terraform variables values
-
-bucket_name = "my-unique-bucket-name"
-
-# New variable value for additional bucket
-additional_bucket_name = "my-additional-bucket-name"
